2d1a63d019ad29eeb0a92c3a2330501b17b9d33e
Edney Oliveira <edney.oliveira.ext@panonline.com.br>
2023-06-13 10:46:49 -0300

    Correcao de erros

--- generators/security/templates/app/src/main/java/domain/adapters/services/ServiceImpl.java
+++ generators/security/templates/app/src/main/java/domain/adapters/services/ServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import <%= packageName %>.application.adapters.mapper.<%= entityName %>Mapper;
 import <%= packageName %>.application.adapters.mapper.Converter;
+import <%= packageName %>.domain.ports.interfaces.<%= entityName %>ServicePort;
 import org.springframework.stereotype.Component;
 
 @Component
